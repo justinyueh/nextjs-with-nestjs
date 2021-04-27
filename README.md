62ab8940cfd4d315dd09e7c696199ef857c656fd
Justin Yueh <justin.yueh@gmail.com>
2021-04-27 12:00:39 +0800

    feat: build

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # nextjs-with-nestjs
+
+## Quick Start
+
+```bash
+yarn
+
+yarn start:dev
+```
+
+## Build
+
+```bash
+yarn build
+```
+
+## Production Start
+
+```bash
+yarn start:prod
+```
--- src/server/modules/view/view.service.ts
+++ src/server/modules/view/view.service.ts
@@ -1,13 +1,15 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import next from 'next';
 
+const dev = process.env.NODE_ENV !== 'production';
+
 @Injectable()
 export class ViewService implements OnModuleInit {
   private server;
 
   async onModuleInit(): Promise<void> {
     try {
-      this.server = next({ dev: true, dir: './src/client' });
+      this.server = next({ dev, dir: './src/client' });
       await this.server.prepare();
     } catch (error) {
       console.log(error);
